dbe8e146cd698cebd45ea4b076ddeb2873b2e96a
unknown <chanyb0827@gmail.com>
2022-06-28 13:02:23 +0900

    add: (function) 확장함수

--- function.kt
+++ function.kt
@@ -2,6 +2,7 @@
  * 함수
  * 1) 오버로딩, 오버라이딩 가능
  * 2) 디폴트 인자
+ * 3) 확장함수
  */
 
 
@@ -33,4 +34,10 @@ fun defaultParameter_start(arg1: Int=532, arg2: Int): Unit {
 fun defaultParameter_end(arg1: Int, arg2: Int=142): Unit {
     println(arg1);
     println(arg2);
+}
+
+// 3) 확장함수
+fun People.nextYear(): Int {
+    // People class에 없던 nextYear 멤버 함수를 추가했음.
+    return this.age+1;
 }
