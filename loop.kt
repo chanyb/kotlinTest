02cd6a80b29ae564d7726f648634fccfac18189d
unknown <chanyb0827@gmail.com>
2022-06-29 16:58:14 +0900

    update: (loop) 루프 함수 명 수정

--- loop.kt
+++ loop.kt
@@ -6,7 +6,7 @@
  */
 
 // 1) 반복문 (upsizing)
-fun loop() {
+fun loop1() {
     for(idx in 0..9) {
         //인덱싱에사용할 idx를 따로 선언하지 않아도 된다.
         // idx in 0..9 를통해 0~9까지 찍을 수 있음.
@@ -15,7 +15,7 @@ fun loop() {
 }
 
 // 2) step
-fun loop() {
+fun loop2() {
     for(idx in 0..9 step 3) {
         // 스텝 설정 가능
         println(idx);
@@ -23,7 +23,7 @@ fun loop() {
 }
 
 // 3) Down Sizing
-fun loop() {
+fun loop3() {
     for(idx in 9 downTo 0) {
         // 9~0으로 내려오기 가능
         println(idx);
