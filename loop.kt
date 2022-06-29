23e5d8084f228fc199522d4ff824e4dc77883808
unknown <chanyb0827@gmail.com>
2022-06-29 16:28:09 +0900

    add: (loop) 반복문 추가

---
+++ loop.kt
@@ -0,0 +1,31 @@
+/*
+ * 반복문
+ * 1) 반복문 (upsizing)
+ * 2) step
+ * 3) downsizing
+ */
+
+// 1) 반복문 (upsizing)
+fun loop() {
+    for(idx in 0..9) {
+        //인덱싱에사용할 idx를 따로 선언하지 않아도 된다.
+        // idx in 0..9 를통해 0~9까지 찍을 수 있음.
+        println(idx);
+    }
+}
+
+// 2) step
+fun loop() {
+    for(idx in 0..9 step 3) {
+        // 스텝 설정 가능
+        println(idx);
+    }
+}
+
+// 3) Down Sizing
+fun loop() {
+    for(idx in 9 downTo 0) {
+        // 9~0으로 내려오기 가능
+        println(idx);
+    }
+}
